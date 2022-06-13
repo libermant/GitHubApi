18efdc49c647b8cc5fdecb60135912aeeb4026e3
libermant <tsipil@webschool.co.il>
2022-06-14 00:37:28 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,72 +1,83 @@
-import {useEffect,useState} from "react";
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Search from "./components/Search/Search";
 import Button from "./components/Button/Button";
 import User from "./components/User/User";
 import Sort from "./components/Sort/Sort";
-import "./App.css"
-
-
+import "./App.css";
 
 function App() {
- const[gitUser,setGitUser] = useState("")
- const[searchUser,setSearchUser] =useState("")
- const[users,setUsers]=useState([]) 
- const[isSearched, setIsSearched] = useState(false); 
- const[sortBy,setSortBy]=useState("")
- 
+  const [gitUser, setGitUser] = useState("");
+  const [searchUser, setSearchUser] = useState("");
+  const [users, setUsers] = useState([]);
+  const [isSearched, setIsSearched] = useState(false);
+  const [sortBy, setSortBy] = useState("");
 
+  //npm i axios
 
- 
- 
-
-
-//npm i axios
-
-  useEffect(()=>{
-    try{
-    async function fetchData(){
-      const gitHubApiUl=`https://api.github.com/users/${searchUser}`
-      if(searchUser==="")return;
-      const {data}=await axios.get(gitHubApiUl)
-      const {avatar_url,created_at,login,public_repos}=data
-      setUsers([...users,{avatar_url,created_at,login,public_repos}])     
-    }
-    fetchData()}catch(e){
+  useEffect(() => {
+    try {
+      async function fetchData() {
+        const gitHubApiUl = `https://api.github.com/users/${searchUser}`;
+        if (searchUser === "") return;
+        const { data } = await axios.get(gitHubApiUl);
+        const { avatar_url, created_at, login, public_repos } = data;
+        setUsers([...users, { avatar_url, created_at, login, public_repos }]);
+      }
+      fetchData();
+    } catch (e) {
       console.log(e);
     }
-  },[searchUser,sortBy])
+  }, [searchUser]);
 
-  
-  
+  useEffect(() => {
+    console.log("sort changed", sortBy);
+  }, [sortBy]);
+
   return (
     <>
-    <Search setGitUser={setGitUser} gitUser={gitUser}/>
+      <Search setGitUser={setGitUser} gitUser={gitUser} />
 
-    <Button text={"Search"}
-     clickEvent={()=>{
-       console.log("Search");
-       setSearchUser(gitUser);
-       setIsSearched(true);
-      console.log(isSearched);
-       setGitUser("")}} />
+      <Button
+        text={"Search"}
+        clickEvent={() => {
+          console.log("Search");
+          setSearchUser(gitUser);
+          setIsSearched(true);
+          console.log(isSearched);
+          setGitUser("");
+        }}
+      />
 
-    <Button text={"Reset"}
-     clickEvent={(e)=>{
-       /*e.target.parentElement.children[5].remove()*/
-       setUsers([])}}/>     
+      <Button
+        text={"Reset"}
+        clickEvent={(e) => {
+          /*e.target.parentElement.children[5].remove()*/
+          setUsers([]);
+        }}
+      />
 
-    <Sort users={users} setGitUser={setGitUser} gitUser={gitUser} sortBy={sortBy} setSortBy={setSortBy}/>    
-    
-    <div id="flex"> 
-      {isSearched? users.map((user)=>      
-       <User avatar_url={user.avatar_url}
-        created_at={user.created_at.split("T")[0]}
-        login={user.login}
-        public_repos={user.public_repos}/>):("")}   
-    </div>
-    
-    </> 
+      <Sort
+        users={users}
+        setGitUser={setGitUser}
+        gitUser={gitUser}
+        sortBy={sortBy}
+        setSortBy={setSortBy}
+      />
+
+      <div id="flex">
+        {isSearched
+          ? users.map((user) => (
+              <User
+                avatar_url={user.avatar_url}
+                created_at={user.created_at.split("T")[0]}
+                login={user.login}
+                public_repos={user.public_repos}
+              />
+            ))
+          : ""}
+      </div>
+    </>
   );
 }
 
--- src/components/Sort/Sort.js
+++ src/components/Sort/Sort.js
@@ -1,61 +1,61 @@
-import React ,{useState,useEffect} from 'react'
-import "./Sort.css"
+import React, { useState, useEffect } from "react";
+import "./Sort.css";
 
-const Sort = ({users,setGitUser,setSortBy,sortBy}) => {
+const Sort = ({ users, setGitUser, setSortBy, sortBy }) => {
+  //const[sortBy,setSortBy]=useState("")
 
-    //const[sortBy,setSortBy]=useState("")
+  function sortDate(a, b) {
+    var dateA = new Date(a.created_at).getTime();
+    var dateB = new Date(b.created_at).getTime();
+    return dateA > dateB ? 1 : -1;
+  }
 
-    function sortDate(a,b){  
-        var dateA = new Date(a.created_at).getTime();
-        var dateB = new Date(b.created_at).getTime();
-        return dateA > dateB ? 1 : -1;  
-      };
+  function sortName(a, b) {
+    let nameA = a.login.toLowerCase();
+    let nameB = b.login.toLowerCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  }
 
-    function sortName (a,b){
-        let nameA = a.login.toLowerCase(); 
-        let nameB = b.login.toLowerCase();                     
-        if (nameA < nameB) {
-            return -1;
-        }
-        if (nameA > nameB) {
-            return 1;
-        }                   
-        return 0;
-    } 
-     
-       
+  const sortManager = (sortOption) => {
+    switch (sortOption) {
+      case "Date":
+        setSortBy(users.sort(sortDate));
+        console.log(users);
+        break;
+      case "Name":
+        setSortBy(users.sort(sortName));
+        console.log(users);
+        break;
+      case "Repositories":
+        setSortBy(users.sort((a, b) => a.public_repos - b.public_repos));
+        console.log(users);
+        break;
+    }
+  };
 
-
-    useEffect(()=>{        
-        switch (sortBy){
-            case "Date":
-                users.sort(sortDate);
-                console.log(users);                
-                break;
-            case "Name":
-                users.sort(sortName)
-                console.log(users);                
-                break;    
-            case "Repositories":
-                users.sort((a,b) => a.public_repos - b.public_repos);
-                console.log(users);                
-                break;
-        }                
-    },[sortBy])
-   
-    
   return (
-    <div className='sort'>
-        <h1>Sort by</h1>
-        <select value={sortBy} onChange={(e)=>{setSortBy(e.target.value);setGitUser("")} }>
-            <option value=""></option>
-            <option value="Date">Date</option>
-            <option value="Name">Name</option>
-            <option value="Repositories">Repositories</option>
-        </select>
-       
+    <div className="sort">
+      <h1>Sort by</h1>
+      <select
+       value={sortBy}
+        onChange={(e) => {
+          sortManager(e.target.value);
+        //   setGitUser("");
+        }}
+      >
+        <option value=""></option>
+        <option value="Date">Date</option>
+        <option value="Name">Name</option>
+        <option value="Repositories">Repositories</option>
+      </select>
     </div>
-  )
-}
+  );
+};
 
-export default Sort
+export default Sort;
